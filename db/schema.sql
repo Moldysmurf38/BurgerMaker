32c5492e9036b1101bae715e31d5adf84f9d58c6
David <davwil38@gmail.com>
2019-06-08 21:12:24 -0400

    v2.0

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,12 @@
 drop database if exists burgers_db;
 create database burgers_db;
 
+use burgers_db;
+
 create table burgers
 (
     id int not null AUTO_INCREMENT,
 	name varchar(255) not null,
 	devoured boolean default false,
 	primary key (id)
-)
-
+);
